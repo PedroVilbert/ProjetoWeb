a12b79a2d4676e5f3f2211b36d639c3b00d084bb
Pedro Vilbert <pedrovilbert2005@gmail.com>
2025-06-08 18:01:40 -0300

    Atualização tela login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path : 'home', component: HomeComponent, canActivate: [AuthGuard]}, 
-  {path : 'cadastro', component: CadastroComponent, canActivate: [AuthGuard]},
+  {path : 'cadastro', component: CadastroComponent},
   {path : 'cadastro/:id', component: CadastroComponent, canActivate: [AuthGuard]}, // edição
   {path : 'consulta', component: ConsultaCepComponent, canActivate: [AuthGuard]},
   {path : 'login', component: LoginComponent},
